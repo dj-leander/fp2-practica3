25483c5881908f84e8f81659b138443ff586d62a
ZunDubCore <stepanstepan0000@gmail.com>
2021-05-25 20:41:20 +0200

    Update main to include some tests for selection sort

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 #include "vector.h"
+#include "selection_sort.h"
 
 void main()
 {
@@ -10,8 +11,29 @@ void main()
 
 	printf("test;\n");
 
-	int *arr = random_vect(1000);
+	int *arr_10 = random_vect(10);
+	int *arr_100 = random_vect(100);
+	int *arr_1000 = random_vect(1000);
+	int *arr_10000 = random_vect(10000);
+	int *arr_100000 = random_vect(100000);
+	
+	unsigned long long n_iters = 0;
+	selection_sort(arr_10, 10, &n_iters);
+	printf("n_iters 10 = %llu\n", n_iters);
 
-	for (unsigned i = 0; i < 1000; i++)
-		printf("%d ", arr[i]);
+	n_iters = 0;
+	selection_sort(arr_100, 100, &n_iters);
+	printf("n_iters 100 = %llu\n", n_iters);
+
+	n_iters = 0;
+	selection_sort(arr_1000, 1000, &n_iters);
+	printf("n_iters 1000 = %llu\n", n_iters);
+
+	n_iters = 0;
+	selection_sort(arr_10000, 10000, &n_iters);
+	printf("n_iters 10000 = %llu\n", n_iters);
+
+	n_iters = 0;
+	selection_sort(arr_100000, 100000, &n_iters);
+	printf("n_iters 100000 = %llu\n", n_iters);
 }
