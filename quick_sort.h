ecf53cd5b35487a6729f633cf7ff5845c9da36ef
Kenneth Leander <55855067+dj-leander@users.noreply.github.com>
2021-05-27 23:30:33 +0200

    FIx quick_sort algorithm
    - Overwrites the natural parameter types in the partition function by integers. Because for some reason there's a time when the search pointer "i" is negative.

--- quick_sort.h
+++ quick_sort.h
@@ -1,7 +1,6 @@
 #pragma once
 
-unsigned int partition(int arr[], unsigned int left, unsigned int right);
-
-// void quick_sort_aux(int arr[], unsigned left, unsigned right, unsigned long long *n_iters);
+int partition(int arr[], int left, int right);
 
 void quick_sort(int arr[], unsigned int size, unsigned long long *n_iters);
+
--- utils.h
+++ utils.h
@@ -3,3 +3,9 @@
 int *random_vect(unsigned int size);
 
 void swap(int *a, int *b);
+
+/* void print_array(int arr[], unsigned int size); */
+
+void print_partial_array(int arr[], unsigned int l, unsigned int r);
+
+void print_entire_array(int arr[], unsigned int size);
