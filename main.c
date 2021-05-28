1618ae3ffb7e31aff86826cab81224de840a7ba7
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2021-05-28 16:43:03 +0200

    Update main

--- main.c
+++ main.c
@@ -1,13 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "utils.h"
-#include "selection_sort.h"
-#include "insertion_sort.h"
-#include "merge_sort.h"
-#include "bubble_sort.h"
-#include "quick_sort.h"
-#include "binary_search.h"
 #include "tests.h"
 
 void main()
@@ -16,15 +11,6 @@ void main()
 
 	printf("Generating random vectors...\n\n");
 
-	int *arr_10 = random_vect(10);
-	int *arr_100 = random_vect(100);
-	int *arr_1000 = random_vect(1000);
-	int *arr_10000 = random_vect(10000);
-	int *arr_100000 = random_vect(100000);
-	
-	unsigned long long n_iters = 0;
-	selection_sort(arr_10, 10, &n_iters);
-	printf("n_iters 10 = %llu\n", n_iters);
 	int *test_arrays[N_MEASUREMENTS][N_SIZES];
 
 	for (int i = 0; i < N_MEASUREMENTS; i++)
@@ -39,65 +25,6 @@ void main()
 
 	printf("Freeing memory...");
 
-	/*
-	n_iters = 0;
-	insertion_sort(arr_100, 100, &n_iters);
-	printf("n_iters 100 = %llu\n", n_iters);
-
-	n_iters = 0;
-	insertion_sort(arr_1000, 1000, &n_iters);
-	printf("n_iters 1000 = %llu\n", n_iters);
-
-	n_iters = 0;
-	insertion_sort(arr_10000, 10000, &n_iters);
-	printf("n_iters 10000 = %llu\n", n_iters);
-
-	n_iters = 0;
-	insertion_sort(arr_100000, 100000, &n_iters);
-	printf("n_iters 100000 = %llu\n", n_iters);
-	*/
-
-	/*
-	n_iters = 0;
-	merge_sort(arr_10, 10, &n_iters);
-	printf("n_iters 10 = %llu\n", n_iters);
-
-	n_iters = 0;
-	bubble_sort(arr_100, 100, &n_iters);
-	printf("n_iters 100 = %llu\n", n_iters);
-
-	n_iters = 0;
-	bubble_sort(arr_1000, 1000, &n_iters);
-	printf("n_iters 1000 = %llu\n", n_iters);
-
-	n_iters = 0;
-	bubble_sort(arr_10000, 10000, &n_iters);
-	printf("n_iters 10000 = %llu\n", n_iters);
-
-	n_iters = 0;
-	bubble_sort(arr_100000, 100000, &n_iters);
-	printf("n_iters 100000 = %llu\n", n_iters);
-	*/ 
-
-	n_iters = 0;
-	quick_sort(arr_10, 10, &n_iters);
-	printf("\nquick_sort n_iters 10 = %llu\n", n_iters); 
-
-	n_iters = 0;
-	quick_sort(arr_100, 100, &n_iters);
-	printf("n_iters 100 = %llu\n", n_iters);
-
-	n_iters = 0;
-	quick_sort(arr_1000, 1000, &n_iters);
-	printf("n_iters 1000 = %llu\n", n_iters);
-
-	n_iters = 0;
-	quick_sort(arr_10000, 10000, &n_iters);
-	printf("n_iters 10000 = %llu\n", n_iters);
-
-	n_iters = 0;
-	quick_sort(arr_100000, 100000, &n_iters);
-	printf("n_iters 100000 = %llu\n", n_iters); 
 	for (int i = 0; i < N_MEASUREMENTS; i++)
 		for (int j = 0; j < N_SIZES; j++)
 			free(test_arrays[i][j]);
