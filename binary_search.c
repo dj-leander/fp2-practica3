23b927e4e4455adb3cd9aed357b289702c77f9d8
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2021-05-31 17:49:17 +0200

    Update binary search algorithm, add a wrapper function

--- binary_search.c
+++ binary_search.c
@@ -1,5 +1,6 @@
 #include "stdio.h"
 #include "binary_search.h"
+
 /*
 	Description:
 		Looks for a number in a sorted array. It applies the binary search
@@ -12,18 +13,21 @@
 		Returns the position where the target number is || -1 in case the number
 		doesn't exist.
 */
-int binary_search(int arr[], int low, int high, int target, unsigned long long *n_iters)
+int binary_search_aux(int arr[], unsigned int low, unsigned int high, int target, unsigned long long *n_iters)
 {
 	unsigned int mid;
 	int mid_value;
+
 	while (low <= high)
 	{
 		++*n_iters;
+
 		mid = (low + high) / 2;
 
-		printf("\nlow = %d\tmid = %d\thigh = %d\ttarget = %d\n", low, mid, high, target);
+		/*printf("\nlow = %d\tmid = %d\thigh = %d\ttarget = %d\n", low, mid, high, target);*/
 
 		mid_value = arr[mid];
+
 		if (target == mid_value) 
 			return mid;
 		if (target > mid_value) 
@@ -31,6 +35,13 @@ int binary_search(int arr[], int low, int high, int target, unsigned long long *
 		else
 			high = mid - 1;
 	}
+
 	/* If number not found return -1 */
 	return -1;
+}
+
+/* Wrapper to call binary search*/
+int binary_search(int arr[], unsigned int size, int target, unsigned long long *n_iters)
+{
+	return binary_search_aux(arr, 0, size - 1, target, n_iters);
 }
--- binary_search.h
+++ binary_search.h
@@ -1,3 +1,5 @@
 #pragma once
 
-int binary_search(int arr[], int low, int high, int target, unsigned long long *n_iters);
+int binary_search_aux(int arr[], unsigned int low, unsigned int high, int target, unsigned long long *n_iters);
+
+int binary_search(int arr[], unsigned int size, int target, unsigned long long *n_iters);
